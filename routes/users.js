b6d12cd7ade919992d0035fd03771056809df3b8
Kunal Londhe <kunal.shahaji.londhe@gmail.com>
2024-06-20 16:44:55 +0530

    new becrypt commit

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 
